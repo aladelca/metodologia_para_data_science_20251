29c507b7a8084b2d4a02dd6a9b7ec20e692ec930
Yesmi Torres <torrespaucaryesmi@gmail.com>
2025-04-22 20:59:22 -0500

    add configuration to oro data

--- src/pipeline/config.py
+++ src/pipeline/config.py
@@ -11,6 +11,7 @@ DATA_DIR.mkdir(parents=True, exist_ok=True)
 URL_BCRP = "https://estadisticas.bcrp.gob.pe/estadisticas/series/api/"
 SERIE_AFP = "PN01178MM/json"
 SERIE_TC = "PD04639PD/json"
+SERIE_ORO = "PN01654XM/json"
 # Diccionario de equivalencias de meses (en mayúsculas)
 EQUIVALENCIAS_MESES = {
     "ENE": "Jan",
@@ -30,3 +31,4 @@ EQUIVALENCIAS_MESES = {
 # Ruta absoluta para el archivo AFP
 RUTA_AFP = str(DATA_DIR / "afp.csv")
 RUTA_TC = str(DATA_DIR / "tc.csv")
+RUTA_ORO = str(DATA_DIR / "oro.csv")
